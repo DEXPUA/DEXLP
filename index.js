b48dee1b98b2aa2dbf8b35d7351e60fdd8f4db8b
DEXPUA <vadim2008kul@gmail.com>
2024-12-23 17:54:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Токен твого бота
-const token = '7766962148:AAHa6D3Tdwdhj1c09bEBq0oZAdM8MR3NlNo'; 
+// Замінити на свій токен
+const token = 'YOUR_BOT_API_TOKEN';
+
+// Створюємо нового бота
 const bot = new TelegramBot(token, { polling: true });
 
-// Привітальне повідомлення при запуску
+// Обробка команди /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привіт! Це твій Telegram бот!');
+  bot.sendMessage(chatId, 'Привіт! Це твій бот!');
 });
 
-// Обробка інших команд
+// Обробка команди /help
 bot.onText(/\/help/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Це твій бот. Напиши /start, щоб почати!');
-}); 
+});
+
+// Експортуємо функцію для обробки запитів на Vercel
+module.exports = (req, res) => {
+  res.status(200).send('Bot is working');
+};
